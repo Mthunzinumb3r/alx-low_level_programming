bcf20df059dfd3a5de492347c1ae3f0181a874bf
Mthunzinumb3r <thulanimthunzimaseko@gmail.com>
2023-08-12 13:12:31 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabets in lowercase and Uppercase
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+        char alpha[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int i;
+
+        for (i = 0; i < 52; i++)
+        {
+                putchar(alpha[i]);
+        }
+        putchar('\n');
+        return (0);
