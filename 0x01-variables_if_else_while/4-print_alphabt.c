44c65537fdcf13d53af903715bb192ab5f632950
Mthunzinumb3r <thulanimthunzimaseko@gmail.com>
2023-08-12 14:08:59 -0700

    When I was having that alphabet soup, I never thought that it would pay of

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabets in lowercase
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+        char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+        int i;
+
+        for (i = 0; i < 26; i++)
+        {
+		if (i != 5 && i != 18)
+			putchar(alpha[i]);
+        }
+        putchar('\n');
+        return (0);
