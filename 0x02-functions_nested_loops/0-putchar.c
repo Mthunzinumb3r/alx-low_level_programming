7fbc7b865c405f2234dc36ad8174c04c0a60e358
Mthunzinumb3r <thulanimthunzimaseko@gmail.com>
2023-08-16 05:50:40 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,16 +8,14 @@
 
 int main() 
 {
-    putchar('_');
-    putchar(' ');
-    putchar('p');
-    putchar('u');
-    putchar('t');
-    putchar('c');
-    putchar('h');
-    putchar('a');
-    putchar('r');
-    putchar('\n');
-    
-    return 0;
+	putchar('_');
+	putchar('p');
+    	putchar('u');
+    	putchar('t');
+    	putchar('c');
+    	putchar('h');
+    	putchar('a');
+    	putchar('r');
+    	putchar('\n');
+	return 0;
 }
