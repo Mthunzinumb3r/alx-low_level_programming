c0e1a25222f38ba0f8967c3a4bcfb4358224ef6c
Mthunzinumb3r <thulanimthunzimaseko@gmail.com>
2023-08-16 07:23:41 -0700

    I sometimes suffer from insomnia.

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,16 +11,16 @@ void print_alphabet(void);
 
 int main(void)
 {
-	print_alphabet(void);
+	print_alphabet();
 	return 0;
 }
 
 void print_alphabet(void)
 {
-    char alpha;
-    for(alpha = 'a'; alpha <= 'z'; alpha++)
-    {
-        putchar(alpha);
-    }
+	char alpha;
+	for(alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+	    putchar(alpha);
+	}
     putchar('\n');
 }
