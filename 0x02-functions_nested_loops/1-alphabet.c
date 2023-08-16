891dc2f40a9b57a03839fb658adae981d17bf76e
Mthunzinumb3r <thulanimthunzimaseko@gmail.com>
2023-08-16 06:30:52 -0700

    I sometimes suffer from insomnia.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+# include <stdio>
+# include <main.h>
+
+/**
+ * main - print all alphabets in lowercase
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	void print_alphabet(void)
+	{
+		char alpha;
+
+		for(alpha = 'a';alpha <= 'z'; alpha++)
+			putchar(aphla);
+	}
+	putchar('\n');
+	return (0);
+}
